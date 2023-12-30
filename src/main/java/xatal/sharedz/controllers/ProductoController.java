c1e3a9ee0f12b19f4184f1745baf5cb5602211d5
edsonmanuelcarballovera <edsonmanuelcarballovera@gmail.com>
2023-12-30 14:17:50 -0600

    added cors filter

--- src/main/java/xatal/sharedz/controllers/ProductoController.java
+++ src/main/java/xatal/sharedz/controllers/ProductoController.java
@@ -1,6 +1,5 @@
 package xatal.sharedz.controllers;
 
-import io.jsonwebtoken.Claims;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,12 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import xatal.sharedz.entities.Producto;
-import xatal.sharedz.security.TokenUtils;
 import xatal.sharedz.services.ProductoService;
 import xatal.sharedz.structures.PublicProducto;
 
