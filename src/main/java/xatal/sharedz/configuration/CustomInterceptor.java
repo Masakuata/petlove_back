599b820c19eb1849b1f59bbd86bd0096fca6e7c0
edsonmanuelcarballovera <edsonmanuelcarballovera@gmail.com>
2023-12-30 13:40:28 -0600

    prevent token check when POST usuario

--- src/main/java/xatal/sharedz/configuration/CustomInterceptor.java
+++ src/main/java/xatal/sharedz/configuration/CustomInterceptor.java
@@ -1,6 +1,5 @@
 package xatal.sharedz.configuration;
 
-import io.jsonwebtoken.Claims;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -8,6 +7,7 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerMapping;
@@ -31,7 +31,7 @@ public class CustomInterceptor implements Filter {
 
     private boolean tokenValid(ServletRequest request) {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        if (!httpRequest.getServletPath().contains("login")) {
+        if (!this.isLogin(httpRequest) && !this.isPostUsuario(httpRequest)) {
             String token = httpRequest.getHeader("Token");
             return token != null
                     && TokenUtils.getTokenClaims(token) != null
@@ -40,9 +40,17 @@ public class CustomInterceptor implements Filter {
         return true;
     }
 
-    private boolean usernameMatchesToken(ServletRequest request, String token) {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        Map pathVars = (Map) httpRequest.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+    private boolean isPostUsuario(HttpServletRequest request) {
+        return request.getServletPath().contains("usuario")
+                && request.getMethod().equals(HttpMethod.POST.name());
+    }
+
+    private boolean isLogin(HttpServletRequest request) {
+        return request.getServletPath().contains("login");
+    }
+
+    private boolean usernameMatchesToken(HttpServletRequest request, String token) {
+        Map pathVars = (Map) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
         return pathVars.containsKey("username")
                 && TokenUtils.getTokenClaims(token).get("username").equals(pathVars.get("username"));
     }
